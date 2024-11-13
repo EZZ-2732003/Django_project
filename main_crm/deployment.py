e536be81eb3bd47d44e2a6feb4afe3b07292ee4d
EZZ-2732003 <156380560+EZZ-2732003@users.noreply.github.com>
2024-11-14 01:31:32 +0200

    azure requirements

---
+++ main_crm/deployment.py
@@ -0,0 +1,46 @@
+
+import os 
+from settings import *
+from main_crm.settings import BASE_DIR, DEBUG
+from.settings import BASE_DIR
+
+ALLOWED_HOSTS =[os.environ['WEBSITE_HOSTNAME']]
+
+CSRF_TRUSTED_ORIGINS=['https://'+os.environ['WEBSITE_HOSTNAME']]
+
+DEBUG = False
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+
+Connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+
+parameters = {pair.split('='):pair.split('=')[1] for pair in Connection_string.split('')}
+
+
+DATABASES ={
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parameters['dbname'],
+        'HOST':parameters['host'],
+        'USER':parameters['user'],
+        'PASSWORD':parameters['password'],
+        
+    }
+    
+}
--- main_crm/wsgi.py
+++ main_crm/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_crm.settings')
+
+settings_module = 'main_crm.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'main_crm.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
